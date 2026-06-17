ab4bf138c84c22fbf3efd1a93f521318cbb5b5ff
lim2222 <johnlky222@gmail.com>
2026-06-17 19:39:31 +0800

    Add individual 4 directions tilt button for wiimote/nunchuk

--- Source/Android/app/build.gradle.kts
+++ Source/Android/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
         versionCode = getBuildVersionCode()
 
-        versionName = getGitVersion() + "-motion 1.5"
+        versionName = getGitVersion() + "-motion 1.6"
 
         buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
         buildConfigField("String", "BRANCH", "\"${getBranch()}\"")
