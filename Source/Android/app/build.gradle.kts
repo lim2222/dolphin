cac7e188170edd9853779d2b93dd976638abc1d8
lim2222 <johnlky222@gmail.com>
2026-06-22 20:16:49 +0800

    Add SwingOnSwipe/NSwingOnSwipe, more single/double-tap button, fix always recenter/IR Mode not refresh upon select

--- Source/Android/app/build.gradle.kts
+++ Source/Android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 
         versionCode = getBuildVersionCode()
 
-        versionName = getGitVersion() + "-motion 1.7"
+        versionName = getGitVersion() + "-motion 1.8"
 
         buildConfigField("String", "GIT_HASH", "\"${getGitHash()}\"")
         buildConfigField("String", "BRANCH", "\"${getBranch()}\"")
